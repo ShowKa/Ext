9876e5b441d3f063b99f2d1f5dc1071a873f2b06
em06 <em06@elt.local>
2020-06-11 10:40:24 +0900

    install kuromoji package

--- content/content.js
+++ content/content.js
@@ -1,7 +1,18 @@
 browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   alert(msg.hello);
+  var tokens = tokenizer.tokenize("すもももももももものうち");
+  tokens.forEach(t => {
+    alert(t);
+  });
 });
 
 browser.storage.local.get("color").then(function (item) {
   document.body.style.border = "5px solid " + item.color;
+});
+
+var dictUrl = browser.runtime.getURL("node_modules/kuromoji/dict/");
+var tokenizer;
+kuromoji.builder({ dicPath: dictUrl }).build(function(err, _tokenizer) {
+  console.log(err);
+  tokenizer = _tokenizer;
 });
